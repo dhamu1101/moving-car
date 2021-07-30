f2bcea5065fbb81cade7ba95b0a5d84a7a598a26
dhamu1101 <86165029+dhamu1101@users.noreply.github.com>
2021-07-30 21:07:55 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<button onclick="document.getElementById('mycar').src='pic_cargo.gif'">go</button>
+
+<img id="mycar" src="pic_bulboff.gif" style="width:100px">
+
+<button onclick="document.getElementById('mycar').src='pic_carstop.gif'">stop</button>
+
+</body>
+</html>
